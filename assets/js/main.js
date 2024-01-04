2d77fe26d9410a880d3068aee6f2236fbb3fffff
jlewis-89 <jlewis21689@live.com>
2024-01-04 12:34:06 +0000

    Updated instructions in HTML, add/remove event listeners for start & stop game

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,37 +1,28 @@
-// $(document).ready(function () {
-
-//     $("#test").click(function () {
-//         $("#test").html("TWO THREE FOUR");
-//     });
-
-//     // $("#intro").click(function () {
-//     //     $("#intro").addClass("hide");
-//     // });
-
-// });
-
+// Prevent Script from Running Before DOM Loaded
 document.addEventListener("DOMContentLoaded", function () {
-
+    // Hide Intro Paragraph to Imporve UI & UX
     let introHide = document.getElementById("intro");
 
     introHide.addEventListener("click", function () {
         introHide.classList.add("hide");
     });
-
+    // Test Script Running
     console.log("Hello World");
 
     // Game Core Logic
     let gameRun = false;
     let cardDeck = [];
     let coTicker = ["TSLA", "MSFT", "AAPL"];
+    // Start Game Function
     let startGame = document.getElementById("start");
     startGame.addEventListener("click", function () {
         gameRun = true;
-        
         startGame.innerHTML = "Stop";
+        startGame.removeEventListener;
+
         let bank = document.getElementById("bank");
         bank.innerHTML = "10000";
-    
+
         // Target Generator
         function newTarget(coTicker) {
             let targetIndex = Math.floor(Math.random() * coTicker.length);
@@ -42,34 +33,41 @@ document.addEventListener("DOMContentLoaded", function () {
         // Update Page with Ticker Target
         let targetCo = document.querySelector("#targetCo");
         targetCo.innerHTML = targetTicker;
-        
+
         let executeBtn = document.getElementById("execute");
-        executeBtn.addEventListener("click", function(){
+        executeBtn.addEventListener("click", function () {
             targetTicker = newTarget(coTicker); // call newTarget function and update target Ticker
             console.log(targetTicker); //testing function
             targetCo.innerHTML = targetTicker;
 
         });
 
+
         //while game is running set target ticker, wait for user select input, start timeout clock function,
         //compare user selections && if user selects pairs in time score ++ else score --
         //Update target ticker
 
-        // while (gameRun) {
-             setTimeout(console.log("Game Running"),10000);
-        // };
-    
-    
+
+        setTimeout(console.log("Game Running"), 10000);
+
+        startGame.addEventListener("click", function () {
+            gameRun = false;
+            startGame.innerHTML = "Start";
+            startGame.removeEventListener;
+        });
+
     });
+
+
 
     // let coIcon = [assets/images/Tesla_Motors.png, assets/images/Microsoft_logo.png, assets/images/Apple_logo_black.png];
 
 
-    
 
-    
 
-    
+
+
+
 
 
 
