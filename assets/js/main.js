25ee85056939a41bbc8e71a537e4fa28dc722f55
jlewis-89 <jlewis21689@live.com>
2024-01-04 12:08:57 +0000

    Additional functionaility to display and update Target element

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,17 +21,59 @@ document.addEventListener("DOMContentLoaded", function () {
     console.log("Hello World");
 
     // Game Core Logic
+    let gameRun = false;
     let cardDeck = [];
-
+    let coTicker = ["TSLA", "MSFT", "AAPL"];
     let startGame = document.getElementById("start");
     startGame.addEventListener("click", function () {
-        startGame.innerHTML = "Reset";
+        gameRun = true;
+        
+        startGame.innerHTML = "Stop";
         let bank = document.getElementById("bank");
         bank.innerHTML = "10000";
+    
+        // Target Generator
+        function newTarget(coTicker) {
+            let targetIndex = Math.floor(Math.random() * coTicker.length);
+            return coTicker[targetIndex];
+        }
+        let targetTicker = newTarget(coTicker);
+        console.log(targetTicker);
+        // Update Page with Ticker Target
+        let targetCo = document.querySelector("#targetCo");
+        targetCo.innerHTML = targetTicker;
+        
+        let executeBtn = document.getElementById("execute");
+        executeBtn.addEventListener("click", function(){
+            targetTicker = newTarget(coTicker); // call newTarget function and update target Ticker
+            console.log(targetTicker); //testing function
+            targetCo.innerHTML = targetTicker;
+
+        });
+
+        //while game is running set target ticker, wait for user select input, start timeout clock function,
+        //compare user selections && if user selects pairs in time score ++ else score --
+        //Update target ticker
+
+        // while (gameRun) {
+             setTimeout(console.log("Game Running"),10000);
+        // };
+    
+    
     });
 
-    let coIcon = [assets/images/Tesla_Motors.png, assets/images/Microsoft_logo.png, assets/images/Apple_logo_black.png];
-    let coTicker = [TSLA, MSFT, AAPL];
+    // let coIcon = [assets/images/Tesla_Motors.png, assets/images/Microsoft_logo.png, assets/images/Apple_logo_black.png];
+
+
+    
+
+    
+
+    
+
+
+
+
 
     // math rand select 1 coIcon, && math rand select 1 coTicker apply to card
 
