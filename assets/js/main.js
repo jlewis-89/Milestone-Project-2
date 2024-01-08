b39d2eac36a7ca7b05b4d2f96705a7620c17b9d5
jlewis-89 <jlewis21689@live.com>
2024-01-08 16:35:17 +0000

    Added timer feature with countdown function

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,13 +3,16 @@ document.addEventListener("DOMContentLoaded", function () {
     // Hide Intro Paragraph to Imporve UI & UX
     let introHide = document.getElementById("intro");
 
+
+    // Hide Instruction paragraph for better UX
     introHide.addEventListener("click", function () {
         introHide.classList.add("hide");
     });
+
     // Test Script Running
     console.log("Hello World");
 
-    // Game Core Logic
+    // Game Core Logic and Variables
     let gameRun = false;
     let cardDeck = [];
     let coTicker = ["TSLA", "MSFT", "AAPL"];
@@ -18,21 +21,29 @@ document.addEventListener("DOMContentLoaded", function () {
     let targetCo = document.querySelector("#targetCo");
     let executeBtn = document.getElementById("execute");
     let inGame = document.getElementById("inGame");
-    
+    let counter = document.getElementById("counter");
+    let moveTime = 0;
+
+        // START OF GAME FUNCTION
+
         function gameFunc(coTicker){
-        
+            
             console.log("gameFunc ran")
             startGame.innerHTML = "Stop";
             bank.innerHTML = "10000";
-            inGame.innerText = "Double Click Stop to Exit!"
-            // startGame.setAttribute("id","stop");
-            // startGame.removeEventListener("click", startGameHandler);
+            inGame.innerText = "Double Click Stop to Exit!";
+            counter.innerHTML = moveTime;
+            updateTimer();
+            
 
+            
+            
             // Target Generator
             function newTarget(coTicker) {
                 let targetIndex = Math.floor(Math.random() * coTicker.length);
                 return coTicker[targetIndex];
             }
+
             let targetTicker = newTarget(coTicker);
             console.log(targetTicker);
             // Update Page with Ticker Target
@@ -42,17 +53,41 @@ document.addEventListener("DOMContentLoaded", function () {
                 targetTicker = newTarget(coTicker); // call newTarget function and update target Ticker
                 console.log(targetTicker); //testing function
                 targetCo.innerHTML = targetTicker;
+                updateTimer();
+                
             });
 
+            setInterval(countDown, 1000); // Call countdown function every second
+
             console.log("Game State is " + gameRun)
-        };
+        }; 
+        
+        // END OF GAMEFUNC
+        
+        // Generate Random Time, check its not 0 (if so make it 10)
+        function updateTimer() {
+        if (moveTime === 0){
+            moveTime = 10;
+        } else { moveTime = Math.floor(Math.random() * 100); }; // Prevent moveTime = 0
+        counter.innerHTML = moveTime; // Update moveTime
+        console.log("move time is " + moveTime);
+    };
     
-    
+    // Countdown the Timer interval
+    function countDown() {
+        moveTime = typeof moveTime === 'number' ? moveTime : 0; //Check moveTime initailised at a number to fix NaN bug
+        moveTime = Math.max(0, moveTime - 1); //minus 1 from moveTime up to a max of 0
+        console.log("countDown called");
+        counter.innerHTML = moveTime; // Update moveTime
+    };
+
+
+    // Call to start / stop game
     let startGameHandler = () => {
         gameRun = !gameRun;
         gameFunc(coTicker);
         console.log("startgame Event Clicked " + gameRun);
-        setTimeout(toggleTest, 1500);  
+        setTimeout(toggleTest, 1500);  // test gameRun state every 1500ms
     };
 
     startGame.addEventListener("click", startGameHandler);
@@ -65,6 +100,7 @@ document.addEventListener("DOMContentLoaded", function () {
         bank.innerHTML = "";
         targetCo.innerHTML = "";
         inGame.innerText = "";
+        counter.innerHTML = "00s";
         console.log("toggle func triggered");
         };
     };
@@ -95,17 +131,23 @@ document.addEventListener("DOMContentLoaded", function () {
             
     //     };
     // };
-
+    
     //counter timing function
-    let counter = document.getElementById("counter");
-    counter.innerHTML = 1000 + "s" //test line
+    
+     //test line
+
+    // while (gameRun === true && startGame.innerHTML != "Start"){
+
+    //     time = Math.floor(Math.random()*10);
+    //     setTimeout(console.log(time),10000);
+    // };
 
     // Scoreboard Functions
 
     // let scoreboard = document.querySelector("id","score");
     // let playerName = document.querySelector("id", "playerName");
     // let playerScore = document.querySelector("id", "playerScore");
-    // playerScore.textContent = "WInner";
+    // playerScore.textContent = "Winner";
     // playerName.textContent = "namanam";
 
 
