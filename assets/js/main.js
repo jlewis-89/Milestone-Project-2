6c946dbed9d50d5fd5bbdf4d4411b284f7a7d0ae
jlewis-89 <jlewis21689@live.com>
2024-01-11 16:22:06 +0000

    Edited HTML, CSS and JS for memory card game

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,57 +18,160 @@ document.addEventListener("DOMContentLoaded", function () {
     let coTicker = ["TSLA", "MSFT", "AAPL"];
     let startGame = document.getElementById("start");
     let bank = document.getElementById("bank");
-    let targetCo = document.querySelector("#targetCo");
+    let targetCo = document.getElementById("targetCo");
     let executeBtn = document.getElementById("execute");
     let inGame = document.getElementById("inGame");
     let counter = document.getElementById("counter");
     let moveTime = 0;
     let pScore = 10000;
+    let gridContainer = document.querySelector(".gridContainer");
+    let firstCard, secondCard;
+    let locked = false;
 
-        // START OF GAME FUNCTION
+    fetch("assets/cards.json")
+        .then((response) => response.json())
+        .then((data) => {
+            cardDeck = [...data, ...data];
+            console.log("fetch read json doc");
+            console.log(data);
+        });
 
-        function gameFunc(coTicker){
+    function shuffleDeck() {
+        let currentIndex = cardDeck.length,
+            randomIndex, temporaryValue;
+        while (currentIndex !== 0) {
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
+            temporaryValue = cardDeck[currentIndex];
+            cardDeck[currentIndex] = cardDeck[randomIndex];
+            cardDeck[randomIndex] = temporaryValue;
+            console.log("deck Shuffled");
+        };
+    };
+
+    function generateCards() {
+        for (let card of cardDeck) {
+            let cardElement = document.createElement("div");
+            cardElement.classList.add("card");
+            cardElement.setAttribute("data-name", card.name);
+            cardElement.innerHTML = `
+            <div class="front">
+                <img class="front-image" src=${card.image} />
+            </div>
+            <div class="back"></div>
+            `;
+            gridContainer.appendChild(cardElement);
+            cardElement.addEventListener("click", flipCard);
+            console.log("cards generated");
+        };
+    };
+
+    function flipCard() {
+        if (locked) return;
+        if (this === firstCard) return;
+
+        this.classList.add("flipped"); // TypeError ???
+
+        if (!firstCard) {
+            firstCard = this;
+            return;
+        }
+
+        secondCard = this;
+        pscore = pScore + 200;
+        locked = true;
+        checkMatch();
+        console.log("flipcard called");
+    };
+
+    function checkMatch() {
+        let isMatch = firstCard.dataset.name === secondCard.dataset.name && firstCard.card.name === targetTicker;
+
+        isMatch ? disableCards() : unflipCards();
+        console.log(firstCard.dataset.name)
+        console.log("check match called");
+    };
+
+    function disableCards() {
+        firstCard.removeEventListener("click", flipCard);
+        secondCard.removeEventListener("click", flipCard);
+        resetBoard();
+        console.log("cards disabled");
+        pScore += 200;
+    };
+
+    function unflipCards() {
+        setTimeout(() => {
+            firstCard.classList.remove("flipped");
+            secondCard.classList.remove("flipped");
+            resetBoard();
+        }, 500);
+        console.log("unflip cards");
+    };
+
+    function resetBoard() {
+        firstCard = null;
+        secondCard = null;
+        locked = false;
+        console.log("reset board called");
+    };
+// Moved Out of Game Function
+    // Target Generator
+    function newTarget(coTicker) {
+        let targetIndex = Math.floor(Math.random() * coTicker.length);
+        return coTicker[targetIndex];
+    }
+    let targetTicker = newTarget(coTicker);
+    console.log(targetTicker);
+    // Update Page with Ticker Target
+    targetCo.innerHTML = targetTicker;
+    executeBtn.addEventListener("click", () => {
+        targetTicker = newTarget(coTicker); // call newTarget function and update target Ticker
+        console.log(targetTicker); //testing function
+        targetCo.innerHTML = targetTicker;
+
+        executeButtonPressed();
+    });
+    // START OF GAME FUNCTION
+
+    function gameFunc(coTicker){
             
-            console.log("gameFunc ran")
-            startGame.innerHTML = "Stop";
-            bank.innerHTML = "1000";
-            inGame.innerText = "Double Click Stop to Exit!";
-            counter.innerHTML = moveTime;
-            updateTimer();
-            
-
-            
-            
-            // Target Generator
-            function newTarget(coTicker) {
-                let targetIndex = Math.floor(Math.random() * coTicker.length);
-                return coTicker[targetIndex];
-            }
-
-            let targetTicker = newTarget(coTicker);
-            console.log(targetTicker);
-            // Update Page with Ticker Target
-            targetCo.innerHTML = targetTicker;
-
-            executeBtn.addEventListener("click", () => {
-                targetTicker = newTarget(coTicker); // call newTarget function and update target Ticker
-                console.log(targetTicker); //testing function
-                targetCo.innerHTML = targetTicker;
-                updateTimer();
-                
-            });
-
-            setInterval(countDown, 1000); // Call countdown function every second
-
-            console.log("Game State is " + gameRun)
-        }; 
+        console.log("gameFunc ran")
+        startGame.innerHTML = "Stop";
+        bank.innerHTML = "10000";
+        inGame.innerText = "Double Click Stop to Exit!";
+        counter.innerHTML = moveTime;
+        updateTimer();
+        shuffleDeck();
+        generateCards();
+        // flipCard();
+        unflipCards();
         
-        // END OF GAMEFUNC
+        
+        
+        
+        console.log(targetTicker);
+
+        function executeButtonPressed (){
+            checkMatch();
+            resetBoard();
+            shuffleDeck();
+            updateTimer();
+        };
+        setInterval(countDown, 1000); // Call countdown function every second
+        console.log("Game State is " + gameRun)
+        
+            
+
+            
+
+            
+    }; // END OF GAMEFUNC
         
         // Generate Random Time, check its not 0 (if so make it 10)
-        function updateTimer() {
+    function updateTimer() {
         if (moveTime === 0){
-            moveTime = 10;
+            moveTime = 15;
         } else { moveTime = Math.floor(Math.random() * 100); }; // Prevent moveTime = 0
         counter.innerHTML = moveTime; // Update moveTime
         console.log("move time is " + moveTime);
@@ -92,8 +195,6 @@ document.addEventListener("DOMContentLoaded", function () {
     };
 
     startGame.addEventListener("click", startGameHandler);
-    
-    
 
     let toggleTest = () =>{
         if (gameRun = !gameRun){
@@ -102,6 +203,7 @@ document.addEventListener("DOMContentLoaded", function () {
         targetCo.innerHTML = "";
         inGame.innerText = "";
         counter.innerHTML = "00s";
+        // resetBoard();
         console.log("toggle func triggered");
         };
     };
@@ -111,15 +213,15 @@ document.addEventListener("DOMContentLoaded", function () {
     console.log(pName);
     playerName.innerHTML = pName;
     pScore = document.getElementById("bank").innerHTML;
-};
+    };
 
     let playerName = document.getElementById("playerName");
 
-if (pScore === 0){
+    if (pScore === 0){
     gameOverLost();
-}else if (pScore === 20000){
+    }else if (pScore === 20000){
     gameOverWin();
-}
+    };
 
     function gameOverLost (){
         alert("Game Over!! Better Luck Next Time")
@@ -129,22 +231,6 @@ if (pScore === 0){
         alert("Congratulations You WON!")
         userName();
     };
-
-// Initalise the Card Deck by grabbing the data from the json file holding tyhe card object and name - Youtube project
-    fetch("assets/cards.json")
-        .then((res) => res.json())
-        .then((data) => {
-            cardDeck = [...data, ...data];
-        });
-
-    
-
-    
-
-    
-
-    
-
 
 }); // End of DOM Loader
 
