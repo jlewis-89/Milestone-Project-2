3283d841438d22d335a9dc6ca5f4921d02ff3bf2
jlewis-89 <jlewis21689@live.com>
2024-01-13 22:55:44 +0000

    Added timer function & shuffle function to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,14 +2,15 @@
 document.getElementById("intro-btn").addEventListener("click", function () {
     document.getElementById("intro").classList.add("hide");
 });
-
+// Game object to hold global variable that the game will edit as needed
 const gameObject = {
     bank: 1000,
     playerName: "",
     playerTime: 0,
+    moveTime: 0,
     updateBankDiv() {
         document.getElementById("bank").innerHTML = this.bank;
-    },
+    }, // game object method
     startGameTime() {
         setInterval(() => {
             this.playerTime += 1;
@@ -27,17 +28,15 @@ document.getElementById("start").addEventListener("click", () => {
     gameObject.getPlayerName();
     gameObject.startGameTime();
     generateCards();
-    // setTimer();
-    // setBank();
+    shuffleCards();
     getTarget();
+    timerFunc();
 })
 
-// if (gameObject.playerName !=""){
-//     document.getElementById("pName").appendChild(`<td>${gameObject.playerName}<td>`);}
-// let cardData = [];
-//generate cards
+// Intialise Empty Card data array to recieve fetch response data
 let cardData = [];
 
+// fetch data from json file
 fetch("./assets/cards.json")
     .then((response) => response.json())
     .then((data) => {
@@ -61,8 +60,6 @@ let generateCards = () => {
     }
 };
 
-//set value in bank to 1000
-
 // display a target
 let getTarget = () => {
     let targetArr = ["TSLA", "AAPL", "MSFT"];
@@ -70,13 +67,39 @@ let getTarget = () => {
     document.getElementById("targetCo").innerHTML = targetArr[targetIndex];
 };
 
-
 // shuffle the deck
+let shuffleCards = () => {
+    let currentIndex = cardData.length,
+        randomIndex, temporaryValue;
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = cardData[currentIndex];
+        cardData[currentIndex] = cardData[randomIndex];
+        cardData[randomIndex] = temporaryValue;
+    };
+};
 
 // start timer
+let timerFunc = () => {
+    gameObject.moveTime = Math.floor(Math.random() * 100);
+    document.getElementById("counter").innerHTML = gameObject.moveTime;
+};
+
+let reduceMoveTime = () => {
+    gameObject.moveTime === typeof "number" ? gameObject.moveTime : 0; // check is a number
+    gameObject.moveTime = Math.max(0, gameObject.moveTime -1); // set limit to avoid negative numbers and reduce by 1
+    document.getElementById("counter").innerHTML = gameObject.moveTime; // update HTML
+};
+
+setInterval(reduceMoveTime,1000); // call function every second
 
 // select cards
 
 // compare cards
 
 // update results
+
+// win loose
+
+// update scoreboard
